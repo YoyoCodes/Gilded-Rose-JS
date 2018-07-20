dd1f9d234ac48a36cd783439c218e03b08cf31fa
Laura Petre <laura.d.petre@gmail.com>
2018-07-20 11:16:38 +0100

    Addled complete name for sulfuras

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,16 +1,21 @@
 describe("Gilded Rose", () => {
 
   describe("Sulfuras", () => {
+    let gildedRose;
+
+    beforeEach(() => {
+      gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 0) ]);
+    });
     it("sellIn doesn't change", () => {
-      const gildedRose = new Shop([ new Item("Sulfuras", 0, 0) ]);
-      const items = gildedRose.updateQuality();
+      let items = gildedRose.updateQuality();
       expect(items[0].sellIn).toEqual(0);
     });
 
     it("quality doesn't change", () => {
-      const gildedRose = new Shop([ new Item("Sulfuras", 0, 0) ]);
-      const items = gildedRose.updateQuality();
+      let items = gildedRose.updateQuality();
       expect(items[0].quality).toEqual(0);
     });
   });
+
+
 });
