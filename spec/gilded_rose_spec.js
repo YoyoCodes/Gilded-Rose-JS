b011a5210c8b84d923459bd38987b02c0eafde5e
Laura Petre <laura.d.petre@gmail.com>
2018-07-22 20:01:26 +0100

    Refactored code by item type

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -39,10 +39,9 @@ describe("Gilded Rose", () => {
     });
 
     it("quality can't be greater than 50", () => {
-      for(let i=0; i < 50; i++){
-          gildedRose.updateQuality();
-      };
-      expect(items[0].quality).toEqual(50);
+      newGildedRose = new Shop([ new Item("Aged Brie", 1, 50) ]);
+      item = newGildedRose.updateQuality();
+      expect(item[0].quality).toEqual(50);
     });
 
     it("quality can't be less than 0", () => {
@@ -76,7 +75,7 @@ describe("Gilded Rose", () => {
     });
 
     it("when sellIn is less than 0, quality drops to 0", () => {
-      for(let i=0; i < 11; i++){
+      for(let i=0; i < 12; i++){
           gildedRose.updateQuality();
       };
       expect(items[0].quality).toEqual(0);
@@ -93,7 +92,7 @@ describe("Gilded Rose", () => {
       items = gildedRose.updateQuality();
     });
     it("sellIn drops by 1", () => {
-      expect(items[0].sellIn).toEqual(1);
+      expect(items[0].sellIn).toEqual(0);
     });
 
     it("quality drops by 2", () => {
